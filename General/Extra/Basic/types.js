37d293bf456fd7c446c6e2692a6ba6227a090d89
Marcelo Oliveira <marcelo.olisi@gmail.com>
2022-11-18 11:41:52 -0300

    types in Js

---
+++ General/Extra/Basic/types.js
@@ -0,0 +1,27 @@
+//Numbers
+
+const n1 = 3;
+const n2 = 4;
+
+let result = n1 * n2;
+
+const list = [10,1, 5, 9, 8, 12, 15];
+
+result = list.sort(); //Don't use, because the Js convert number in text and 80 > 9. Always, considerer the first caracter in text.
+
+result = list.sort((a,b) => a - b); //Correct. Pattern documented: compare number and add an index a each number.
+
+console.log(result);
+
+//String
+
+const cifrao = '\u0024'
+
+console.log(cifrao);
+
+//Bool
+
+const textN1 = "Marcelo";
+const textN2 = "m";
+
+console.log(textN1 > textN2);
