7cf3ce3dffe8028b6df80312039439e4c76be5f4
Marcelo Oliveira <marcelo.olisi@gmail.com>
2022-11-19 14:31:26 -0300

    Create spread operator

--- General/Extra/Basic/sets.js
+++ General/Extra/Basic/sets.js
@@ -15,10 +15,43 @@ const gradesUpdated = grades.map((grade) => {
 const names = ["Ana", "Marcos", "Maria", "Mauro"];
 const averages = [7, 4.5, 8, 7.5];
 
-const disapproved = names.filter((student, index) => {
+const disapproved = names.filter((_, index) => {
     return averages[index] < 7;
 });
 
-console.log(disapproved);
+// _ -> diz que não está utilizando o primeiro parâmetro, porque somos obrigados a passar o nome pra acessar o segundo parêmetro
+
+// console.log(disapproved);
+
+
+//Reduce
+
+const jsClass = [7, 8, 9];
+const csharpClass = [10, 9, 10];
+const pythonClass = [4, 4, 4];
+
+function calculateAverage(gradesClass){
+    const sumGrades = gradesClass.reduce((acc, grade) => acc + grade, 0);
+
+    const average = sumGrades / gradesClass.length;
+
+    return average;
+}
+
+// console.log(`\nThe average of Python class is ${calculateAverage(pythonClass)}\n`)
+
+// Spread Operator (operador de espalhamento) [... array]
+
+const gradesUniversity = [7, 8, 7];
+
+const gradesDuplicated = [...gradesUniversity];
+
+gradesDuplicated.push(8);
+
+console.log(gradesDuplicated);
+console.log(gradesUniversity);
+
+
+
 
 
